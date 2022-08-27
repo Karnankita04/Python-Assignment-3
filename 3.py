75170ef17485140a2c45cb9cac054df76547cdd9
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2022-08-27 14:43:04 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,76 @@
+# 1. Write a python script to convert a number into str type.
+# code: 
+# a=9
+# a=str(a)
+# print(type(a))
+
+# next>-----------------------------------------------------
+
+# 2. Write a python script to print Unicode of the character ‘m’
+# code:
+# print(ord('m'))
+
+# next>-----------------------------------------------------
+
+# 3. Write a python script to print character representation of a given unicode 100.
+# code:
+# print(chr(100))
+
+# next>-----------------------------------------------------
+
+# 4. Write a python script to print any number and its binary equivalent
+# code:
+# x=25
+# print(x,"=",bin(x))
+
+# next>-----------------------------------------------------
+
+# 5. Write a python script to print any number and its octal equivalent.
+# code:
+# x=34
+# print(x,"=",oct(x))
+
+# next>------------------------------------------------------
+
+# 6. Write a python script to print any number and its hexadecimal equivalent.
+# code:
+# x=9
+# print(x,"=",hex(x))
+
+# next>-------------------------------------------------------
+
+# 7. Write a python script to store binary number 1100101 in a variable and print it in
+# decimal format.
+# code:
+# x=0b1100101
+# print(x)
+
+# next>--------------------------------------------------------
+
+
+# 8. Write a python script to store a hexadecimal number 2F in a variable and print it in
+# octal format.
+# code:
+# x=0x2F
+# print(oct(x))
+
+# next>---------------------------------------------------------
+
+# 9. Write a python script to store an octal number 125 in a variable and print it in binary
+# format.
+# code:
+# x=0o125
+# print(bin(x))
+
+# next>-------------------------------------------------------------
+
+# 10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and
+# display the result in binary format.
+# code:
+# x=0o25  
+# y=0x39  
+# print(bin(x) + bin(y))  
+
+
+
+
